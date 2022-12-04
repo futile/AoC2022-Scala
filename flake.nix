0d68d416ee1b7b6513c36af53083501b27340c9d
Felix Rath <felixm.rath@gmail.com>
2022-12-04 22:43:07 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Flake for building my AoC2022 code using nix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";
+  };
+
+  outputs = { self, nixpkgs }@inputs:
+    let
+      supportedSystems = [ "x86_64-linux" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+    in
+    {
+      devShell = forAllSystems
+        (system:
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+            };
+          in
+            pkgs.mkShellNoCC {
+              nativeBuildInputs  = with pkgs; [ ];
+            }
+        );
+    };
+}
